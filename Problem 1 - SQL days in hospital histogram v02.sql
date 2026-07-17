db65bc9b21d29d908056442d6a4eb7ffa6d15c98
bradwarfield <bradwarfield@gmail.com>
2026-07-16 20:04:31 -0400

    MySQL code to create a histogram
    This histogram shows the distribution for the length of stay in the hospital. The dataset only includes stays of at least 1 day and at most 14 days.

---
+++ Problem 1 - SQL days in hospital histogram v02.sql
@@ -0,0 +1,13 @@
+/* Query to find distribution of number of days spent in the hospital by
+creating a quick histogram to show a distribution of the number of days each hospital stay was */
+
+USE patient;
+
+SELECT 
+    ROUND(time_in_hospital, 1) AS days_in_hospital,            /* Rounding to the nearest day */
+    COUNT(*) AS encounters,                                    
+    RPAD('', COUNT(*) / 500, '*') AS '* = 500 encounters'      /* Divide by 500 so each astricks reps 500 patient encounters */
+FROM
+    health
+GROUP BY days_in_hospital
+ORDER BY days_in_hospital;
