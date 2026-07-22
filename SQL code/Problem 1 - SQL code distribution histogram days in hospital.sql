2248b45752e012795742f1f40dbbab36b804737e
bradwarfield <bradwarfield@gmail.com>
2026-07-22 07:02:03 -0400

    Changed code from round to cast
    There was a round, but the data was all whole numbers. However, they are varchar type. The rounding made them numerical.
    Removed the round and added cast as unsigned.

--- SQL code/Problem 1 - SQL code distribution histogram days in hospital.sql
+++ SQL code/Problem 1 - SQL code distribution histogram days in hospital.sql
@@ -4,7 +4,7 @@ creating a quick histogram to show a distribution of the number of days each hos
 USE patient;
 
 SELECT 
-    ROUND(time_in_hospital, 1) AS days_in_hospital,            /* Rounding to the nearest day */
+    CAST(time_in_hospital AS UNSIGNED) AS days_in_hospital,    /* time_in_hospital is varchar type */
     COUNT(*) AS encounters,                                    
     RPAD('', COUNT(*) / 500, '*') AS '* = 500 encounters'      /* Divide by 500 so each astricks reps 500 patient encounters */
 FROM
