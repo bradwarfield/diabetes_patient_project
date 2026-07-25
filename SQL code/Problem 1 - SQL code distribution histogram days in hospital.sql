dc46494996b27d6386098058707dc4bbf0cf3546
bradwarfield <bradwarfield@gmail.com>
2026-07-25 18:59:41 -0400

    Formatting changes

--- SQL code/Problem 1 - SQL code distribution histogram days in hospital.sql
+++ SQL code/Problem 1 - SQL code distribution histogram days in hospital.sql
@@ -3,15 +3,12 @@ creating a quick histogram to show a distribution of the number of days each hos
 
 USE patient;
 
-SELECT
-	/* Casting because time_in_hospital is varchar type */
-    CAST(time_in_hospital AS UNSIGNED) AS days_in_hospital,    
-    COUNT(*) AS encounters,
-    /* Dividing by 500 so each astricks reps 500 patient encounters */
-    RPAD('', COUNT(*) / 500, '*') AS '* = 500 encounters'      
-FROM
-    health
-GROUP BY 
-	days_in_hospital
-ORDER BY 
-	days_in_hospital;
+SELECT	
+		/* Casting because time_in_hospital is varchar type */
+    	CAST(time_in_hospital AS UNSIGNED) AS days_in_hospital,    
+    	COUNT(*) AS encounters,
+    	/* Dividing by 500 so each astricks reps 500 patient encounters */
+    	RPAD('', COUNT(*) / 500, '*') AS '* = 500 encounters'      
+FROM	health
+GROUP BY days_in_hospital
+ORDER BY days_in_hospital;
